a2f059713bd754887bd2c6d8095429cbe03c27e1
Conno <nissimconnor@gmail.com>
2026-09-05 23:01:45 -0700

    Restore functional pre-multiplayer build
    Preserve the complete multiplayer implementation on wip/multiplayer and restore the repository contents to the last build before multiplayer was introduced (1228c55).

--- game.js
+++ game.js
@@ -31,7 +31,7 @@ Module.expectedDataFileDownloads++;
       throw "using preloaded data can only be done on a web page or in a web worker";
     }
     var PACKAGE_NAME = "game.data";
-    var REMOTE_PACKAGE_BASE = "game.data?v=9136bf28";
+    var REMOTE_PACKAGE_BASE = "game.data?v=4e38bdee";
     if (
       typeof Module["locateFilePackage"] === "function" &&
       !Module["locateFile"]
@@ -368,14 +368,14 @@ Module.expectedDataFileDownloads++;
     }
   };
   loadPackage({
-    package_uuid: "200bb52e-0c1a-5ddf-a910-8afbf45b70c8",
-    remote_package_size: 15058650,
+    package_uuid: "4e38bdee-fdab-4087-9141-864efbf80dfc",
+    remote_package_size: 10658810,
     files: [
       {
         filename: "/game.love",
         crunched: 0,
         start: 0,
-        end: 15058650,
+        end: 10658810,
         audio: false,
       },
     ],
